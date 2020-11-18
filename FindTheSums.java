c9269e94690d7f2b6c076e0a8f61fb847e8e7c17
Matt Morris <mattmorris120@gmail.com>
2020-11-18 12:28:23 -0500

    Added some graphical looks, removed driver

--- FindTheSums.java
+++ FindTheSums.java
@@ -1,30 +1,37 @@
+import java.util.Scanner;
+
 /*
  * FindTheSums.java
  * Author:  Matt Morris
  * Submission Date:  4/21/2020
  *
  *Takes a 2d array of Integers ( 1-9 ) and a "Sum Number".
- *Returns a new array, similiar to the first, however if the numbers do not consecutively add up to the Sum Number, the number will be replaced with a 0. 
+ *Returns a new array, similiar to the first, however if the numbers do not consecutively add up to the given Target Sum Number, the number will be replaced with a 0. 
  *Works both vertically and Horizontally, when specified. 
- *
- *
- *
- * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither
- * received nor given inappropriate assistance. I have not copied
- * or modified code from any source other than the course webpage
- * or the course textbook. I recognize that any unauthorized
- * assistance or plagiarism will be handled in accordance with
- * the University of Georgia's Academic Honesty Policy and the
- * policies of this course. I recognize that my work is based
- * on an assignment created by the Department of Computer
- * Science at the University of Georgia. Any publishing 
- * or posting of source code for this assignment is strictly
- * prohibited unless you have written consent from the Department
- * of Computer Science at the University of Georgia.  
+   
  */
 public class FindTheSums {
+	
+	private static Scanner input = new Scanner(System.in); // Scanner for User Input
+	private static boolean on = true; // ON/OFF Switch for CMD Reader.
+	
+	private static int[][] array1 = { 
+		 { 3, 2, 1, 1 },
+		 { 2, 5, 6, 2 }, 
+		 { 1, 2, 9, 8 } 
+		};
+	private static int[][] array2 = {
+		{ 7, 3, 8, 5, 6, 7, 4, 1, 9, 5 }, 
+		{ 8, 1, 6, 1, 8, 4, 6, 9, 9, 6 },
+		{ 9, 2, 4, 8, 6, 1, 1, 3, 6, 2 }, 
+		{ 3, 6, 8, 3, 1, 9, 2, 7, 9, 6 }, 
+		{ 5, 7, 7, 6, 3, 5, 6, 4, 2, 1 },
+		{ 6, 4, 5, 5, 7, 6, 8, 1, 9, 7 }, 
+		{ 8, 4, 5, 4, 3, 7, 1, 2, 1, 8 }, 
+		{ 6, 8, 6, 7, 8, 6, 2, 4, 6, 2 },
+		{ 7, 8, 6, 8, 3, 8, 2, 2, 8, 5 }, 
+		{ 8, 7, 7, 6, 6, 2, 9, 9, 5, 8 } 
+		};
 
 	public static String arrayToString(int[][] a) {
 		String str = "";
@@ -61,10 +68,11 @@ public class FindTheSums {
 		for (int rows = 0; rows < a.length; rows++) { // TRAVERSES THE ROWS
 
 			for (int cols = 0; cols < a[0].length; cols++) { // TRAVERSES THE COLUMNS
-				int sum = 0;
+				int sum = 0; // sets sum and counter ints to 0.
 				int counter = 0;
 
-				for (int i = cols; i < a[0].length; i++) { // Uses cols as a counter of where it is.
+				for (int i = cols; i < a[0].length; i++) { // This is essentially the MATH ENGINE that finds the
+															// consecutive numbers that add up to a Sum.
 					if (sum < sumToFind) { // if sum is still smaller than the number,
 						sum += a[rows][i]; // add the number to sum .
 						counter++; // counter increases. (Counter is important as it keeps track how far i've
@@ -74,8 +82,9 @@ public class FindTheSums {
 				if (sum == sumToFind) { // If the sum DOES = the number
 
 					for (int j = 0; j < counter; j++) {
-						b[rows][cols + j] = a[rows][cols + j]; // fill in B with the respecting details AT the correct
-																// spot.
+						b[rows][cols + j] = a[rows][cols + j]; // fill in B with the respecting details( the numbers
+																// that DO add up to the SUM) at their respective (
+																// original ) locations in the new array.
 
 					}
 				}
@@ -86,9 +95,9 @@ public class FindTheSums {
 	}
 
 	public static int[][] verticalSums(int[][] a, int sumToFind) {
-		// This is essentially the exact same thing as HORIZONTALSUMS, but the rows and
-		// cols are switched.
-		int[][] b = new int[a.length][a[0].length]; // This creates a new Array B, fills it with 0s.
+		// This is essentially the exact same thing as HORIZONTALSUMS, however I
+		// reversed the order for in which it is traversed, ENGINE is adjusted as so.
+		int[][] b = new int[a.length][a[0].length];
 		for (int r = 0; r < a.length; r++) {
 			for (int c = 0; c < a[r].length; c++) {
 				b[r][c] = 0;
@@ -100,7 +109,7 @@ public class FindTheSums {
 				int sum = 0;
 				int counter = 0;
 
-				for (int i = r; i < a.length; i++) {
+				for (int i = r; i < a.length; i++) { // ENGINE , i is set to rows (r) instead of cols .
 					if (sum < sumToFind) {
 						sum += a[i][c];
 						counter++;
@@ -119,8 +128,94 @@ public class FindTheSums {
 
 	}
 
+	public static void cmdReader() {
+		System.out.println("Options:  (1)  Display Array1  (2)  Display Array2  (3)  VerticalSums  (4)  HorizontaSums  (5)  Quit");
+		System.out.println("Type 1, 2, 3, 4, 5 respectively...");
+		int answer = input.nextInt();
+		if (answer == 1) {
+			System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n\n \n \n \n \n \n \n \n\n \n \n \n");
+			System.out.println(arrayToString(array1));
+		}
+		if (answer == 2) {
+			System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n\n \n \n \n \n \n \n \n\n \n \n \n");
+			System.out.println(arrayToString(array2));
+		}
+		if (answer == 3) {
+			runVerticalSums(); 
+			
+		}
+		if (answer == 4) {
+			System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n\n \n \n \n \n \n \n \n\n \n \n \n");
+			System.out.println("Getting there4");
+		}
+		if (answer == 5) {
+			System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n\n \n \n \n \n \n \n \n\n \n \n \n");
+			System.out.println("Program Closing... Have a good day.") ; 
+			on = false;
+			System.exit(0);
+		}
+    }
+
+	public static void runVerticalSums(){ 
+		System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n\n \n \n \n \n \n \n \n\n \n \n \n");
+		System.out.println("Which array would like to run the verticalSums operation on?\n(1) Array1  (2) Array2" );
+		boolean valid = true; 
+		int finalrvsAnswer = 0 ; 
+		while ( valid ){ 
+			int rvsAnswer = input.nextInt() ;
+			
+			if (rvsAnswer != 1 || rvsAnswer != 2 ) { 
+			System.out.println("Invalid number. Please Enter 1 or 2") ; 
+			
+			}
+			else{
+				valid =false ; 
+				finalrvsAnswer = rvsAnswer; 
+			}
+		}
+		 
+	
+		if  ( finalrvsAnswer ==1 ) {
+			System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n\n \n \n \n \n \n \n \n\n \n \n \n");
+			System.out.println("Array1 chosen. What is your target sum?") ; 
+			
+			int numAnswer= input.nextInt() ; 
+			
+			System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n\n \n \n \n \n \n \n \n\n \n \n \n");
+			System.out.println("Running verticalSums with Array1, Target Number: "+numAnswer + "...") ; 
+			System.out.println("Before: \n" + arrayToString(array1)) ; 
+			System.out.println("After: \n"+ arrayToString(verticalSums(array1, numAnswer)) )  ; 
+			System.out.println("\n \n");
+		}
+		if (finalrvsAnswer == 2) { 
+			System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n\n \n \n \n \n \n \n \n\n \n \n \n");
+			System.out.println("Array2 chosen. What is your target sum?") ; 
+			
+			int numAnswer= input.nextInt() ; 
+			
+			System.out.print("\n \n \n \n \n \n \n \n \n \n \n \n\n \n \n \n \n \n \n \n\n \n \n \n");
+			System.out.println("Running verticalSums with Array2, Target Number: "+numAnswer + "...") ; 
+			System.out.println("Before: \n" + arrayToString(array2)) ; 
+			System.out.println("After: \n"+ arrayToString(verticalSums(array2, numAnswer)) )  ; 
+			System.out.println("\n \n");
+
+		}
+	}
+
+
+
 	public static void main(String args[]) {
-//For Testing. 
+		System.out.println("Welcome to FindTheSums, please choose your option:"); 
+		
+		while (on) { 
+			cmdReader(); 
+		}
+	
+
+
+
+
+
 
 	}
 
